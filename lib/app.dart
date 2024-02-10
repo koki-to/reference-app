4e8598349262c06d0a80ae90a391fca1001ed65e
鳥越幸樹 <niaoyuexingshu@gmail.com>
2024-02-10 22:00:40 +0900

    feat: auto_routerの追加

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
-import 'view/todo_list/todo_list_page.dart';
+import 'config/router.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    final appRouter = AppRouter();
+    return MaterialApp.router(
+      routerConfig: appRouter.config(),
       title: 'referenceApp',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const TodoListPage(),
     );
   }
 }
---
+++ lib/view/login/login_page.dart
@@ -0,0 +1,91 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+@RoutePage()
+class LoginPage extends ConsumerWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // ref.listen(loginControllerProvider, (_, state) async {
+    //   if (state.isLoading) {
+    //     ref.watch(overlayLoadingProvider.notifier).update((state) => true);
+    //     return;
+    //   }
+    //   await state.when(
+    //     data: (_) async {
+    //       ref.watch(overlayLoadingProvider.notifier).update((state) => false);
+    //       await context.router.replace(const HomeRoute());
+    //     },
+    //     error: (e, s) async {
+    //       ref.watch(overlayLoadingProvider.notifier).update((state) => false);
+    //       ref
+    //           .read(scaffoldMessengerServiceProvider)
+    //           .showSnackBar('エラーが発生しました。');
+    //     },
+    //     loading: () async {
+    //       ref.watch(overlayLoadingProvider.notifier).update((state) => true);
+    //     },
+    //   );
+    // });
+
+    // final state = ref.watch(loginControllerProvider);
+    final emialController = TextEditingController();
+    final passwordController = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'ログイン画面',
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: TextFormField(
+                controller: emialController,
+                onChanged: (value) {},
+                decoration: const InputDecoration(
+                  hintText: 'メールアドレス',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: TextFormField(
+                controller: passwordController,
+                onChanged: (value) {},
+                decoration: const InputDecoration(
+                  hintText: 'パスワード',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            const SizedBox(height: 60),
+            // ElevatedButton(
+            //   onPressed: state.isLoading
+            //       ? null
+            //       : () async =>
+            //           ref.read(loginControllerProvider.notifier).login(
+            //                 email: emialController.text,
+            //                 password: passwordController.text,
+            //               ),
+            //   child: const Text('ログインする'),
+            // ),
+            // const SizedBox(height: 20),
+            // ElevatedButton(
+            //   onPressed: state.isLoading
+            //       ? null
+            //       : () => context.router.push(const SignupRoute()),
+            //   child: const Text('新規登録'),
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
